7bc46b441d46cb518a27f2c2526c27d7d40441b1
Lauer Robert (XC-DA/ESC2) <Robert.Lauer@bosch.com>
2022-02-13 14:24:52 +0100

    update runExecutable.py that removes executable after successful comparison to avoid comparing old code if build fails and thus no new executable got build

--- GoldMaster/runExecutable.py
+++ GoldMaster/runExecutable.py
@@ -3,17 +3,23 @@ import subprocess
 from filecmp import cmp
 import os
 
+# set up paths
 goldMaster = "../GoldMaster/GoldMaster.txt"
 goldMasterComparator = "../GoldMaster/GoldMasterComparator.txt"
+executable = "../C++/GameRunner.exe"
 
+# run executable and accumulate output
 goldMasterComparatorFile = open(goldMasterComparator, "w")
-subprocess.call(["../C++/GameRunner.exe", "0", "1", "2", "3", "5", "7", "11", "13", "17"], stdout=goldMasterComparatorFile)
+subprocess.call([executable, "0", "1", "2", "3", "5", "7", "11", "13", "17"], stdout=goldMasterComparatorFile)
 goldMasterComparatorFile.close()
 
+# check whether output matches goldMaster
 result = cmp(goldMaster, goldMasterComparator)
 print(result)
 
+# clean up
 if (result is True):
     os.remove(goldMasterComparator)
 else:
     subprocess.call(["C:/Program Files/Beyond Compare 4/BCompare.exe", goldMaster, goldMasterComparator])
+os.remove(executable)
